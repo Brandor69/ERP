49926d4a57bc6bdd6044acd76e3132c938ef2744
BRANDOR69 <alexbrans_rodas95@hotmail.com>
2016-11-04 23:08:10 -0600

    sklsasdklsdfkllasdfkñkljasd

--- Modulo_Produccion/Modulo_Produccion/Form1.Designer.cs
+++ Modulo_Produccion/Modulo_Produccion/Form1.Designer.cs
@@ -28,12 +28,372 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabVerRec = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabAgregarRec = new System.Windows.Forms.TabPage();
+            this.tabModRec = new System.Windows.Forms.TabPage();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTPrep = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabVerRec.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabAgregarRec.SuspendLayout();
+            this.tabModRec.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabVerRec);
+            this.tabControl1.Controls.Add(this.tabAgregarRec);
+            this.tabControl1.Controls.Add(this.tabModRec);
+            this.tabControl1.Location = new System.Drawing.Point(31, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(763, 389);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabVerRec
+            // 
+            this.tabVerRec.Controls.Add(this.button1);
+            this.tabVerRec.Controls.Add(this.textBox5);
+            this.tabVerRec.Controls.Add(this.dataGridView1);
+            this.tabVerRec.Location = new System.Drawing.Point(4, 22);
+            this.tabVerRec.Name = "tabVerRec";
+            this.tabVerRec.Padding = new System.Windows.Forms.Padding(3);
+            this.tabVerRec.Size = new System.Drawing.Size(755, 363);
+            this.tabVerRec.TabIndex = 1;
+            this.tabVerRec.Text = "Ver Recetas";
+            this.tabVerRec.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(616, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(48, 23);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(562, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column5,
+            this.Column6,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(48, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 300);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ClickBoton);
+            // 
+            // tabAgregarRec
+            // 
+            this.tabAgregarRec.Controls.Add(this.label1);
+            this.tabAgregarRec.Controls.Add(this.textBox3);
+            this.tabAgregarRec.Controls.Add(this.label4);
+            this.tabAgregarRec.Controls.Add(this.textBox4);
+            this.tabAgregarRec.Controls.Add(this.label3);
+            this.tabAgregarRec.Controls.Add(this.textBox2);
+            this.tabAgregarRec.Controls.Add(this.label2);
+            this.tabAgregarRec.Controls.Add(this.textBox1);
+            this.tabAgregarRec.Location = new System.Drawing.Point(4, 22);
+            this.tabAgregarRec.Name = "tabAgregarRec";
+            this.tabAgregarRec.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAgregarRec.Size = new System.Drawing.Size(755, 363);
+            this.tabAgregarRec.TabIndex = 0;
+            this.tabAgregarRec.Text = "Agregar Receta";
+            this.tabAgregarRec.UseVisualStyleBackColor = true;
+            // 
+            // tabModRec
+            // 
+            this.tabModRec.Controls.Add(this.button3);
+            this.tabModRec.Controls.Add(this.button2);
+            this.tabModRec.Controls.Add(this.label8);
+            this.tabModRec.Controls.Add(this.txtAutor);
+            this.tabModRec.Controls.Add(this.label5);
+            this.tabModRec.Controls.Add(this.txtTPrep);
+            this.tabModRec.Controls.Add(this.label6);
+            this.tabModRec.Controls.Add(this.txtPrecio);
+            this.tabModRec.Controls.Add(this.label7);
+            this.tabModRec.Controls.Add(this.txtNombre);
+            this.tabModRec.Location = new System.Drawing.Point(4, 22);
+            this.tabModRec.Name = "tabModRec";
+            this.tabModRec.Padding = new System.Windows.Forms.Padding(3);
+            this.tabModRec.Size = new System.Drawing.Size(755, 363);
+            this.tabModRec.TabIndex = 2;
+            this.tabModRec.Text = "Modificar Receta";
+            this.tabModRec.UseVisualStyleBackColor = true;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Precio";
+            this.Column2.Name = "Column2";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Tiempo de Preparación";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Autor";
+            this.Column6.Name = "Column6";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "";
+            this.Column3.Name = "Column3";
+            this.Column3.UseColumnTextForButtonValue = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "";
+            this.Column4.Name = "Column4";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(665, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 31);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Nombre";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(541, 121);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(181, 20);
+            this.txtAutor.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(474, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Autor";
+            // 
+            // txtTPrep
+            // 
+            this.txtTPrep.Location = new System.Drawing.Point(161, 121);
+            this.txtTPrep.Name = "txtTPrep";
+            this.txtTPrep.Size = new System.Drawing.Size(134, 20);
+            this.txtTPrep.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Tiempo de Preparación";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(541, 28);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(125, 20);
+            this.txtPrecio.TabIndex = 21;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(474, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Precio";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(161, 28);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(251, 20);
+            this.txtNombre.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(539, 149);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(472, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Autor";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(159, 149);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Tiempo de Preparación";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(539, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Precio";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nombre";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "Aceptar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(822, 463);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Producción";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabVerRec.ResumeLayout(false);
+            this.tabVerRec.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabAgregarRec.ResumeLayout(false);
+            this.tabAgregarRec.PerformLayout();
+            this.tabModRec.ResumeLayout(false);
+            this.tabModRec.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabAgregarRec;
+        private System.Windows.Forms.TabPage tabVerRec;
+        private System.Windows.Forms.TabPage tabModRec;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewButtonColumn Column3;
+        private System.Windows.Forms.DataGridViewButtonColumn Column4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTPrep;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNombre;
     }
 }
 
--- Modulo_Produccion/Modulo_Produccion/Form1.cs
+++ Modulo_Produccion/Modulo_Produccion/Form1.cs
@@ -2,19 +2,143 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Modulo_Produccion
 {
     public partial class Form1 : Form
     {
+        private SqlConnection conexion;
+        private SqlCommand cmd;
+        //private SqlDataReader read;
+        public static String dat = "server = localhost; database = restaurante; Uid = root; pwd = 1234;";
+        public static String nombre = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private SqlConnection ObtenerConexion() {
+
+            conexion = new SqlConnection();
+            conexion.ConnectionString = dat;
+            conexion.Open();
+            return conexion;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add("Receta1","Q. "+50,"20 Min","yo");
+            dataGridView1.Rows.Add("Receta2", "Q. "+55, "25 Min", "tu");
+            dataGridView1.Rows.Add("Receta3", "Q. " + 60, "30 Min", "ella");
+        }
+
+        private void mostrarReceta() {
+
+            DataSet ds = new DataSet();
+            cmd = new SqlCommand("Select nombre, precio, tiempo_preparacion, autor from Receta",ObtenerConexion());
+            SqlDataAdapter recetas = new SqlDataAdapter(cmd);
+            recetas.Fill(ds);
+            dataGridView1.DataSource=ds.Tables[1];
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ClickBoton(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.ColumnIndex == 4)
+            {
+                var row = dataGridView1.CurrentRow;
+
+                tabControl1.SelectedTab = tabModRec;
+                var nombre = tabModRec.Controls["txtNombre"];
+                var precio = tabModRec.Controls["txtPrecio"];
+                var tiempo = tabModRec.Controls["txtTPrep"];
+                var autor = tabModRec.Controls["txtAutor"];
+
+                nombre.Text = row.Cells[0].Value.ToString();
+                precio.Text = row.Cells[1].Value.ToString();
+                tiempo.Text = row.Cells[2].Value.ToString();
+                autor.Text = row.Cells[3].Value.ToString();
+
+
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Receta Modificada Exitosamente");
+
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtTPrep.Text = "";
+            txtAutor.Text = "";
+
+            tabControl1.SelectedTab = tabVerRec;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = "";
+            txtPrecio.Text = "";
+            txtTPrep.Text = "";
+            txtAutor.Text = "";
+
+            tabControl1.SelectedTab = tabVerRec;
         }
     }
 }
